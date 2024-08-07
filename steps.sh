c537349ac4b51822b5ca436bcfef38dfd53f67a3
Neivanny1 <nevileneivanny@gmail.com>
2024-08-07 17:08:26 +0300

    updates

--- steps.sh
+++ steps.sh
@@ -1,7 +1,10 @@
+# DATABASE
 docker network create database
 docker run --name my-postgres -e POSTGRES_USER=backend -e POSTGRES_PASSWORD=backend -d --network=database postgres
+#BACKEND
 docker build -t todo_backend:v1 .
 docker run --name todo_backend -p 8000:8000 --network=database -d todo_backend:v1
+docker exec -it todo_backend python3 manage.py migrate
 
 # FRONTEND BUILDS
 docker build -t todo_fronted:v1 .
